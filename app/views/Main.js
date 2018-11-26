7e58cf783fe9ceac9c985b0899dce6d5f9ec0f1e
Sean Chang <seantc@uci.edu>
2018-11-25 18:39:02 -0800

    Modified and added basic project files

---
+++ app/views/Main.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Main extends Component<{}> {
+  render() {
+    return (
+      <Text>Hello World</Text>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Main from './app/views/Main.js';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
