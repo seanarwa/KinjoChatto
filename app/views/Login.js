22fa8655b57ac9fe5ac44dac5e2462f139aa66dd
Sean Chang <seanarwa@gmail.com>
2018-12-19 17:20:30 -0800

    Add app views

---
+++ app/views/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+ type Props = {};
+ export default class Login extends Component<Props> {
+
+   render() {
+     return (
+       <View>
+
+       </View>
+     );
+   }
+ }
---
+++ app/views/NewChat.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+ type Props = {};
+ export default class NewChat extends Component<Props> {
+
+   render() {
+     return (
+       <View>
+
+       </View>
+     );
+   }
+ }
