4cf54d67798313684bb298f12fcdf50212eefa64
Sean Chang <seanarwa@gmail.com>
2018-12-19 18:29:33 -0800

    Modified Map component

---
+++ app/components/main/Map.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+// import MapView from 'react-native-maps';
+
+ type Props = {};
+ export default class Map extends Component<Props> {
+
+   render() {
+     return (
+       <View>
+       <Text>Hello</Text>
+       </View>
+     );
+   }
+ }
+
+ // <MapView
+ //   initialRegion={{
+ //     latitude: 37.78825,
+ //     longitude: -122.4324,
+ //     latitudeDelta: 0.0922,
+ //     longitudeDelta: 0.0421,
+ //   }}
+ // />
