553d453d9270ab6db4616047f1812fb8e567123f
Feasting <andrewtnguyen49@gmail.com>
2018-12-20 16:09:17 -0800

    Login UI

--- app/views/Login.js
+++ app/views/Login.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+    ScrollView,
+    Text,
+    TextInput,
+    View,
+    Button
+} from 'react-native';
 
- type Props = {};
- export default class Login extends Component<Props> {
+export default class Login extends Component {
 
-   render() {
-     return (
-       <View>
-
-       </View>
-     );
-   }
- }
+    render() {
+        return (
+            <ScrollView style={{padding: 20}}>
+                <Text 
+                    style={{fontSize: 27}}>
+                    Login
+                </Text>
+                <TextInput placeholder='Username' />
+                <TextInput placeholder='Password' />
+                <View style={{margin:7}} />
+                <Button 
+                          onPress={this.props.onLoginPress}
+                          title="Submit"
+                      />
+            </ScrollView>
+            )
+    }
+}
